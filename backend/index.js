f7061643385806aaff666426f6c5f4049650936a
NammikaParthiv <nammikaparthiv@gmail.com>
2026-05-14 21:54:23 +0530

    fixed and updated cors

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://sms-sage-mu.vercel.app",
+    credentials: true,
+  }),
+)
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
